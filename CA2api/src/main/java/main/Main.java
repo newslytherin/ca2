859d6d2b09186863b8167863cb8e28905f8c47a1
NikolajX4000 <supernikolaj@hotmail.com>
2018-10-02 13:05:39 +0200

    entity classes

---
+++ CA2api/src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import javax.persistence.Persistence;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Persistence.generateSchema("pu", null);
+    }
+}
