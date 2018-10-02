028edf296a3909d0b32b85f4ffeb61061ae0e3bf
NikolajX4000 <supernikolaj@hotmail.com>
2018-10-02 17:28:09 +0200

    PersonDTO

--- CA2api/src/main/java/facade/PersonFacade.java
+++ CA2api/src/main/java/facade/PersonFacade.java
@@ -1,5 +1,7 @@
 package facade;
 
+import entity.PersonDTO;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -14,9 +16,37 @@ public class PersonFacade {
     public void addEntityManageractory(EntityManagerFactory emf) {
         this.emf = emf;
     }
+
     //get all + filter
-    //get by id
-    //get by phone
-    //get by email
-//    public PersonDTO 
+    public PersonDTO getPersonDTOByPhone(int id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.createNamedQuery("Person.findbyid", PersonDTO.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+    public PersonDTO getPersonDTOByPhone(String phone) {
+        EntityManager em = emf.createEntityManager();
+        String jpql = "SELECT new PersonDTO(e) FROM Person e WHERE (SELECT p.number FROM e.phones p) = :phone";
+        try {
+            return em.createQuery(jpql, PersonDTO.class).setParameter("number", phone).getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+    public PersonDTO getPersonDTOByEmail(String email) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.createNamedQuery("Person.findbyemail", PersonDTO.class)
+                    .setParameter("email", email)
+                    .getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
 }
