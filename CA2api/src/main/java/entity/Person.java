7f14e7936d39f6390b4f843bb13613dd8197e99c
NikolajX4000 <supernikolaj@hotmail.com>
2018-10-02 14:34:12 +0200

    PersonDTO

--- CA2api/src/main/java/entity/Person.java
+++ CA2api/src/main/java/entity/Person.java
@@ -5,31 +5,34 @@ import java.util.List;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
-/**
- *
- * @author super
- */
 @Entity
 @DiscriminatorValue("P")
+@NamedQueries(
+        @NamedQuery(name = "Person.findbyemail", query = "SELECT p FROM Person where p.email = :email")
+)
 public class Person extends InfoEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
     private String firstName;
     private String lastName;
     @ManyToMany(mappedBy = "people")
     @ElementCollection()
     private List<Hobby> hobbies;
 
-    public Integer getId() {
-        return id;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public List<Hobby> getHobbies() {
+        return hobbies;
     }
 
 }
