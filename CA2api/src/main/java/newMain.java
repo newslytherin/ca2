622a80d8520fb2b61a89be0e832d6fc156858ada
NikolajX4000 <supernikolaj@hotmail.com>
2018-10-02 10:49:28 +0200

    source package fix

---
+++ CA2api/src/main/java/newMain.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author super
+ */
+public class newMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
